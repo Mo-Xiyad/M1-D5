c2a95998a3e7902419a652fe4e0ed1d2d14de5be
Zee <xziyad92@gmail.com>
2021-08-15 21:35:13 +0200

    #Extra questions

--- extra.js
+++ extra.js
@@ -86,6 +86,66 @@ const maxShoppingCart = function (shoppingCart) {
 console.log(maxShoppingCart(shoppingCart), `\n`)
 
 
+
+
+
+/* EXERCISE 15
+In your eCommerce you have an array of objects called shoppingCart. In this array you have a number of objects with a price, a name, an id and the quantity to be shipped.
+Create a function "latestShoppingCart" which receives the shoppingCart array and returns the last item.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 16
+Create a function "loopUntil" which receives an integer x between 0 and 9.
+The function loops and prints a random number between 0 and 9 until the random number is bigger than x for three times in a row.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 17
+Write a function "average" which receives an array and return the average value. The function automatically skips non-numeric entries in the array.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 18
+Write a function "longest" to find the longest string from an given array of strings.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 19
+Write a function to create a very simple anti spam filter for your mailbox. The function takes a string emailContent, and returns a boolean.
+Check if the email is valid using string methods. The email (in this example) is valid if the words SPAM and SCAM does not appear.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 20
+Write a function that receives a date d as parameter and calculates the number of days passes since the d.
+*/
+
+/* WRITE YOUR CODE HERE */
+
+/* EXERCISE 21
+Write a function "matrixGenerator" that receives x and y as parameter. The result should be a matrix of x times y with, as value, the index of the position.
+Ex.: X = 3, Y = 2
+["00","01","02"
+"10","11","12"]
+*/
+
+
+
+
+
+
+
+
+
+
+
+
 // #########################  Extra  Methods of Array  ###########################
 separatorWithHEading('array.filter()')
 
